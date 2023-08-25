639a82efc6ddaf7bf702546a7db8ccd75be9b192
Vadim Koenen <lekovad@gmail.com>
2023-08-25 23:57:45 +0300

    VM2

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,8 +5,8 @@ import { CurrentUserContext } from '../contexts/CurrentUserContext';
 function Card({ card, onCardClick, onCardLike, onCardDelete, openConfirmPopup }) {
   const { name, link, likes, owner } = card;
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = owner._id === currentUser._id;
-  const isLiked = likes.some((i) => i._id === currentUser._id);
+  const isOwn = owner === currentUser._id;
+  const isLiked = likes.some((i) => i === currentUser._id);
 
   const cardLikeButtonClassName = `element__like-button ${isLiked && "element__like-button_active"
     }`;
